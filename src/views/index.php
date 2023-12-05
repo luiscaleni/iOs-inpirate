5bdc06c0cb9fba57b07dd16ea4fedb88c1f55ec2
Luis Caleni <luis.caleni@gmail.com>
2023-12-05 05:49:39 -0300

    commit solucionado

--- src/views/index.php
+++ src/views/index.php
@@ -6,7 +6,7 @@ use Luis\Petslife\views\layouts\Foot;
 $head = new Head;
 echo $head->index();
 
-    include("src/views/components/apps.php");
+    include("src/views/components/apps.php"); 
 
 $foot = new Foot;
 echo $foot->index();
